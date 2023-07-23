602387e4a4846b565c3dbad80bf5549fa97ab74c
Chistyakov Alexey <chskela@gmail.com>
2023-07-23 23:06:39 +0300

    Updated State - update gallows type on Gallows

--- src/main/kotlin/State.kt
+++ src/main/kotlin/State.kt
@@ -2,5 +2,5 @@ data class State(
     val word: String = "мама",
     val mask: String = "*".repeat(word.length),
     val attempts: Int = 0,
-    val gallows: String = ""
+    val gallows: Gallows = Gallows.Default
 )
