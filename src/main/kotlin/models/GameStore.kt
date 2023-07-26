d7f5948935115369e3d6c4f62bdaf3eb2c096e64
Chistyakov Alexey <chskela@gmail.com>
2023-07-26 18:04:52 +0300

    Added GameStore - game store

---
+++ src/main/kotlin/models/GameStore.kt
@@ -0,0 +1,13 @@
+package models
+
+class GameStore {
+
+    private var _gameState: GameState = GameState.Menu()
+
+    fun setGameState(gameState: GameState) {
+        _gameState = gameState
+    }
+
+    fun getGameState()= _gameState
+
+}
