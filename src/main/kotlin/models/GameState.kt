95becd047707f80660a5d976653c2fbb1e666686
Chistyakov Alexey <chskela@gmail.com>
2023-08-09 23:15:32 +0300

    Updated GameState - remove gallows from Process

--- src/main/kotlin/models/GameState.kt
+++ src/main/kotlin/models/GameState.kt
@@ -1,14 +1,11 @@
 package models
 
-import views.Gallows
-
 sealed class GameState {
     data class Menu(var isExit: Boolean = false) : GameState()
     data class Process(
-        val word: String = "мама",
+        val word: String,
         val mask: String = word.replace(Regex("[А-яЁё]"), "*"),
         val attempts: Int = 0,
-        val gallows: Gallows = Gallows.Default
     ) : GameState()
 
     data class Win(val word: String) : GameState()
