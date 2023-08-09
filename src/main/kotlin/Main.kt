1f67c0576193f88b760f46cb2ec06a2bb7983d6b
Chistyakov Alexey <chskela@gmail.com>
2023-08-10 00:15:46 +0300

    Updated main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,16 @@
 import Settings.MAX_NUMBER_OF_ERRORS
 import Settings.PATH_FILE
-import presenters.GamePresenter
 import models.GameStore
 import models.WordStore
+import presenters.GamePresenter
 import views.ConsoleDisplay
 
 fun main() {
 
     val game = GamePresenter(
         wordStore = WordStore(pathFile = PATH_FILE),
-        gameStore = GameStore(),
-        gameDisplay = ConsoleDisplay(),
-        maxNumberOfErrors = MAX_NUMBER_OF_ERRORS
+        gameStore = GameStore(maxNumberOfErrors = MAX_NUMBER_OF_ERRORS),
+        gameDisplay = ConsoleDisplay()
     )
 
     game.loop()
