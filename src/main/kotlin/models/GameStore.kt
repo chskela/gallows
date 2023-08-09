01314d711c0364480a0488b2735d1a6736141950
Chistyakov Alexey <chskela@gmail.com>
2023-08-10 00:13:40 +0300

    Updated GameStore - create fun handlerEnteredLetter and support fun's

--- src/main/kotlin/models/GameStore.kt
+++ src/main/kotlin/models/GameStore.kt
@@ -1,6 +1,6 @@
 package models
 
-class GameStore(private var _gameState: GameState = GameState.Menu()) {
+class GameStore(private var _gameState: GameState = GameState.Menu(), private val maxNumberOfErrors: Int) {
 
     fun setGameState(gameState: GameState) {
         _gameState = gameState
@@ -8,4 +8,37 @@ class GameStore(private var _gameState: GameState = GameState.Menu()) {
 
     fun getGameState()= _gameState
 
+    fun handlerEnteredLetter(letter: Char) {
+        when (val state = _gameState) {
+
+            is GameState.Process -> {
+                val newState = if (state.word.contains(letter)) {
+                    state.copy(mask = updateMask(state, letter))
+                } else {
+                    state.copy(attempts = state.attempts + 1)
+                }
+                checkGameState(newState)
+            }
+
+            else -> _gameState
+        }
+    }
+
+    private fun checkGameState(newState: GameState.Process) {
+        _gameState = when {
+            isWin(newState) -> GameState.Win(word = newState.word)
+
+            isGameOver(newState) -> GameState.GameOver(word = newState.word)
+
+            else -> newState
+        }
+    }
+
+    private fun isWin(state: GameState.Process) = state.mask == state.word
+
+    private fun isGameOver(state: GameState.Process) = state.attempts >= maxNumberOfErrors
+
+    private fun updateMask(state: GameState.Process, letter: Char) = state.mask.mapIndexed { index, c ->
+        if (letter == state.word[index]) letter else c
+    }.joinToString("")
 }
