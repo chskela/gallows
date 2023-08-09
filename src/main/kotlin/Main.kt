23597af5ad037b2266fa220349b853d44b726595
Chistyakov Alexey <chskela@gmail.com>
2023-08-09 23:22:28 +0300

    Created ConsoleDisplay - impl interface GameDisplay

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,14 +3,14 @@ import Settings.PATH_FILE
 import presenters.GamePresenter
 import models.GameStore
 import models.WordStore
-import views.GameDisplay
+import views.ConsoleDisplay
 
 fun main() {
 
     val game = GamePresenter(
         wordStore = WordStore(pathFile = PATH_FILE),
         gameStore = GameStore(),
-        gameDisplay = GameDisplay(),
+        gameDisplay = ConsoleDisplay(),
         maxNumberOfErrors = MAX_NUMBER_OF_ERRORS
     )
 
---
+++ src/main/kotlin/views/ConsoleDisplay.kt
@@ -0,0 +1,73 @@
+package views
+
+class ConsoleDisplay : GameDisplay {
+
+    override fun showMenu() {
+        print(
+            """
+            Здравствуйте, это игра "Виселица",
+            если хотите сыграть введите "Y",
+            для выхода введите "N"
+            Ввод: 
+        """.trimIndent()
+        )
+    }
+
+    override fun showLetsPlay() {
+        println("Сыграем!!!")
+    }
+
+    override fun showGoodbye() {
+        println("До свидания!!!")
+    }
+
+    override fun showErrorInputCommand() {
+        println("Команда не распознана! Пожалуйста, попробуйте еще раз")
+    }
+
+    override fun showGameState(mask: String, attempts: Int) {
+        val gallows = getNewGallows(attempts)
+
+        print(
+            """
+            Загаданное слово: ${mask.uppercase().split("").joinToString(" ")}
+            Ошибок: $attempts
+            Виселица: $gallows
+            Введите букву: 
+        """.trimIndent()
+        )
+    }
+
+    override fun showErrorInputLetter() {
+        println("Буква не распознана! Пожалуйста, попробуйте еще раз")
+    }
+
+    override fun showWin(word: String){
+        println("""
+            
+            Поздравляю, Вы победили!!!
+            Загаданное слово: ${word.uppercase()}
+            
+        """.trimIndent())
+    }
+
+    override fun showGameOver(word: String){
+        println("""
+            
+            К сожалению, Вы проиграли!!!
+            Виселица: ${Gallows.RightLeg.view}
+            Загаданное слово: ${word.uppercase()}
+            
+        """.trimIndent())
+    }
+
+    private fun getNewGallows(attempts: Int) = when (attempts) {
+        1 -> Gallows.Had
+        2 -> Gallows.Torso
+        3 -> Gallows.LeftHand
+        4 -> Gallows.RightHand
+        5 -> Gallows.LeftLeg
+        6 -> Gallows.RightLeg
+        else -> Gallows.Default
+    }
+}
